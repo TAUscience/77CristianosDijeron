7dc5a2a59d89b83487ed128d0b3820a4db592f67
TAUscience <139826086+TAUscience@users.noreply.github.com>
2024-04-28 05:14:07 -0600

    Cambio en BD

--- js/data.js
+++ js/data.js
@@ -9,7 +9,7 @@ const DB_PREGUNTA = [
     {id: 7, pregunta: "Nombra uno de los libros del Pentateuco.", respuesta1: "Génesis", respuesta2: "Éxodo", respuesta3: "Deuteronomio", valor1: 44, valor2: 22, valor3: 11},
     {id: 8, pregunta: "Menciona uno de los dones espirituales mencionados en el Nuevo Testamento.", respuesta1: "Sanidad o liberación", respuesta2: "Sabiduría", respuesta3: "Enseñanza", valor1: 39, valor2: 23, valor3: 15},
     {id: 9, pregunta: "Menciona un instrumento musical utilizado en la adoración en el Antiguo Testamento.", respuesta1: "Arpa", respuesta2: "Pandero", respuesta3: "Shofar", valor1: 60, valor2: 9, valor3: 8},
-    {id: 10, pregunta: "Nombra uno de los doce jueces que gobernaron a Israel antes de la era de los reyes.", respuesta1: "Débora", respuesta2: "Sansón", respuesta3: "Gedeón o Jerobaal", valor1: 42, valor2: 18, valor3: 17},
+    {id: 10, pregunta: "Nombra uno de los doce jueces que gobernaron a Israel antes de la era de los reyes.", respuesta1: "Débora", respuesta2: "Sansón", respuesta3: "Gedeón", valor1: 42, valor2: 18, valor3: 17},
     {id: 11, pregunta: "Menciona una de las siete iglesias a las que se dirigen las cartas en el libro de Apocalipsis.", respuesta1: "Éfeso", respuesta2: "Filadelfia", respuesta3: "Pérgamo", valor1: 48, valor2: 19, valor3: 10},
     {id: 12, pregunta: "Menciona uno de los animales que aparecen en la visión de Daniel sobre los cuatro reinos.", respuesta1: "León", respuesta2: "Oso", respuesta3: "Águila", valor1: 48, valor2: 19, valor3: 10},
     {id: 13, pregunta: "Nombra uno de los libros poéticos del Antiguo Testamento.", respuesta1: "Cantares", respuesta2: "Salmos", respuesta3: "Proverbios", valor1: 42, valor2: 26, valor3: 9},
@@ -17,7 +17,7 @@ const DB_PREGUNTA = [
     {id: 15, pregunta: "Nombres de reyes del pueblo de Dios.", respuesta1: "David", respuesta2: "Salomón", respuesta3: "Saúl", valor1: 51, valor2: 17, valor3: 9},
     {id: 16, pregunta: "Nombra uno de los hijos o hijas de David.", respuesta1: "Salomón", respuesta2: "Absalón", respuesta3: "Tamar", valor1: 51, valor2: 17, valor3: 9},
     {id: 17, pregunta: "Personajes que recibieron un milagro de Jesús.", respuesta1: "Lázaro", respuesta2: "Leproso", respuesta3: "Suegra de Pedro", valor1: 39, valor2: 20, valor3: 18},
-    {id: 18, pregunta: "Menciona una de las fiestas celebradas por el pueblo de Israel en el Antiguo Testamento.", respuesta1: "Pascua (Pesaj)", respuesta2: "Pentecostés (Shavuot)", respuesta3: "Tabernáculos (Sukkot)", valor1: 59, valor2: 12, valor3: 6},
+    {id: 18, pregunta: "Menciona una de las fiestas celebradas por el pueblo de Israel en el Antiguo Testamento.", respuesta1: "Pascua", respuesta2: "Pentecostés", respuesta3: "Tabernáculos", valor1: 59, valor2: 12, valor3: 6},
     {id: 19, pregunta: "Menciona uno de los objetos sagrados dentro del tabernáculo en el desierto.", respuesta1: "Candelero de oro", respuesta2: "Mesa de los panes", respuesta3: "Arca del pacto", valor1: 38, valor2: 26, valor3: 13},
     {id: 20, pregunta: "Menciona uno de los diez mandamientos.", respuesta1: "No robarás", respuesta2: "No matarás", respuesta3: "No tendrás dioses ajenos", valor1: 38, valor2: 29, valor3: 10},
     {id: 21, pregunta: "Menciona alguno de los falsos dioses mencionados en la Biblia.", respuesta1: "Baal", respuesta2: "Becerro de oro", respuesta3: "Astoret", valor1: 38, valor2: 20, valor3: 19}
